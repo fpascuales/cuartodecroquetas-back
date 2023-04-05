e79d5924e03e45735c31194b369b4fbb4aa689b8
fpascuales <fpascuales@gmail.com>
2023-04-05 14:10:03 +0200

    Actualización orders

--- src/api/orders/orders.model.js
+++ src/api/orders/orders.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose")
 
 const orderSchema = new mongoose.Schema(
     {
-        croqueta: [{type: mongoose.Types.ObjectId, ref: "croquetas"}],
-        // quantity: {type: Number, required: true},
-        // subtotal: {type: Number, required: true}
+        croqueta: {type: mongoose.Types.ObjectId, ref: "croquetas"},
+        quantity: {type: Number, required: true},
+        subtotal: {type: Number, required: true}
     },
     {
         timestamps: true,
