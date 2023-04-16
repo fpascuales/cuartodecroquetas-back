6291075075949d472c7c6308ac169e1dbbd1bbff
fpascuales <fpascuales@gmail.com>
2023-04-16 21:31:18 +0200

    Create updateCroqueta()

--- src/api/croquetas/croquetas.routes.js
+++ src/api/croquetas/croquetas.routes.js
@@ -6,7 +6,7 @@ const croquetasRoutes = require("express").Router()
 croquetasRoutes.get("/", getAllCroquetas)
 croquetasRoutes.get("/:id", getCroquetasById)
 croquetasRoutes.post("/", [isAuth], createCroqueta)
-croquetasRoutes.put("/", [isAuth], updateCroqueta)
+croquetasRoutes.put("/:id", [isAuth], updateCroqueta)
 croquetasRoutes.delete("/:id", [isAuth], deleteCroqueta)
 
 module.exports = croquetasRoutes
